7b4c0e861a3e2c1c338525c80999f2f9441ce9f0
Bran <1055025755@qq.com>
2021-12-30 14:07:01 +0800

    feat: lint 忽略配置

--- typings.d.ts
+++ typings.d.ts
@@ -1,10 +1,20 @@
+declare module 'slash2';
 declare module '*.css';
 declare module '*.less';
-declare module '*.png';
+declare module '*.scss';
+declare module '*.sass';
 declare module '*.svg' {
-  export function ReactComponent(
-    props: React.SVGProps<SVGSVGElement>,
-  ): React.ReactElement;
+  export function ReactComponent(props: React.SVGProps<SVGSVGElement>): React.ReactElement;
   const url: string;
   export default url;
 }
+declare module '*.png';
+declare module '*.jpg';
+declare module '*.jpeg';
+declare module '*.gif';
+declare module '*.bmp';
+declare module '*.tiff';
+declare module 'omit.js';
+
+declare const REACT_APP_ENV: 'test' | 'dev' | 'pre' | false;
+declare const REACT_APP_API_URL: string; // 接口地址的基础路径
