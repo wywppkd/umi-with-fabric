5926cf1b6a7cca4a49285c7b105e030afadc7e0a
Bran <1055025755@qq.com>
2022-04-23 16:10:26 +0800

    lint: change config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,11 @@
 module.exports = {
   extends: [require.resolve('@umijs/fabric/dist/eslint')],
 
+  // 根据个人习惯自定义rule
   rules: {
-    // your rules
+    'no-console': 'off',
+    'react-hooks/exhaustive-deps': 'error', // react-hooks 依赖检查
+    'no-empty': 'off', // catch{} 允许为空
+    '@typescript-eslint/no-shadow': ['off'], // 当前作用域变量名不能与父级作用域变量同名
   },
 };
--- typings.d.ts
+++ typings.d.ts
@@ -1,20 +1,18 @@
-declare module 'slash2';
 declare module '*.css';
 declare module '*.less';
 declare module '*.scss';
 declare module '*.sass';
-declare module '*.svg' {
-  export function ReactComponent(props: React.SVGProps<SVGSVGElement>): React.ReactElement;
-  const url: string;
-  export default url;
-}
 declare module '*.png';
 declare module '*.jpg';
 declare module '*.jpeg';
 declare module '*.gif';
 declare module '*.bmp';
-declare module '*.tiff';
-declare module 'omit.js';
+declare module '*.svg' {
+  export function ReactComponent(props: React.SVGProps<SVGSVGElement>): React.ReactElement;
+  const url: string;
+  export default url;
+}
 
+// 自定义全局变量类型
 declare const REACT_APP_ENV: 'test' | 'dev' | 'pre' | false;
 declare const REACT_APP_API_URL: string; // 接口地址的基础路径
