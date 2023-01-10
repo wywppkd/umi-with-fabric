dc4e220b608ea75962f6300f6c06f3f9cb01d5df
Bran <1055025755@qq.com>
2023-01-10 15:45:38 +0800

    eslint: 一个组件一个文件

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,5 +7,6 @@ module.exports = {
     'react-hooks/exhaustive-deps': 'error', // react-hooks 依赖检查
     'no-empty': 'off', // catch{} 允许为空
     '@typescript-eslint/no-shadow': ['off'], // 当前作用域变量名不能与父级作用域变量同名
+    'react/no-multi-comp': 'error',
   },
 };
