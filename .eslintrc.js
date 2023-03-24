8c9e1bf5cb04507b8fc53305666657d01dabb02a
Bran <1055025755@qq.com>
2023-03-24 09:42:45 +0800

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,5 +8,6 @@ module.exports = {
     'no-empty': 'off', // catch{} 允许为空
     '@typescript-eslint/no-shadow': ['off'], // 当前作用域变量名不能与父级作用域变量同名
     'react/no-multi-comp': 'error',
+    '@typescript-eslint/no-unused-vars': 'off',
   },
 };
