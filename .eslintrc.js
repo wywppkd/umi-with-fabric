458b4107dfc8f10f4ee059eafbdc75a8213b0111
Bran <1055025755@qq.com>
2023-03-31 14:06:08 +0800

    config

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,5 +9,6 @@ module.exports = {
     '@typescript-eslint/no-shadow': ['off'], // 当前作用域变量名不能与父级作用域变量同名
     'react/no-multi-comp': 'error',
     '@typescript-eslint/no-unused-vars': 'off',
+    'react/self-closing-comp': 'off',
   },
 };
