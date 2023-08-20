09c5f95d47b4fac2ce27be7e20e9bb3f5cf9bd37
Bran <1055025755@qq.com>
2023-08-20 20:18:21 +0800

    add com1

---
+++ src/components/helloWorld.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+type Props = {};
+
+const HelloWorld = (props: Props) => {
+  return <div>HelloWorld</div>;
+};
+
+export default HelloWorld;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import styles from './index.less';
+import HelloWorld from '@/components/helloWorld';
 
 export default () => {
   return (
     <div>
+      <HelloWorld />
       <h1 className={styles.title}>Page index</h1>
     </div>
   );
