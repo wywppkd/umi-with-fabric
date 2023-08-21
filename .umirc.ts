17c54d0656f8fba19dc822759c635ddf8dad3c92
Bran <1055025755@qq.com>
2023-08-21 17:07:51 +0800

    feat: add gh-pages

--- .umirc.ts
+++ .umirc.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'umi';
+import { name } from './package.json';
 
 export default defineConfig({
   nodeModulesTransform: {
     type: 'none',
   },
+  publicPath: process.env.NODE_ENV === 'production' ? `/${name}/` : '/',
 });
